510b5e2f661b105bfc5b026b4372b57ae4731930
fenq2 <fenq2@ukr.net>
2023-04-20 16:32:53 +0300

    progress commit

--- src/js/main.js
+++ src/js/main.js
@@ -650,26 +650,56 @@ const minPopup = () => {
 
 minPopup();
 
-const time = 10000;
-const step = 1;
-
-function outNum(num, elem) {
-  let e = document.querySelector(elem);
-  let n = 0;
-  let t = Math.round(time / (num / step));
-  let interval = setInterval(() => {
-    n = n + step;
-    if (n == num) {
-      clearInterval(interval);
+(function () {
+  let counter = document.querySelectorAll('.number');
+  let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+  window.addEventListener('scroll', function () {
+    if (limit == counter.length) {
+      return;
     }
-    e.innerHTML = n;
-  }, t);
-}
 
-outNum(2005, '.number');
-outNum(50, '.number1');
-outNum(1500, '.number2');
-outNum(100, '.number3');
+    for (let i = 0; i < counter.length; i++) {
+      let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+      let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+      if (pos < win && counter[i].dataset.stop === '0') {
+        counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+        let x = 0;
+        limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+        let int = setInterval(function () {
+          // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+          x = x + Math.ceil(counter[i].dataset.to / 50);
+          counter[i].innerText = x;
+          if (x > counter[i].dataset.to) {
+            //Как только досчитали - стираем интервал.
+            counter[i].innerText = counter[i].dataset.to;
+            clearInterval(int);
+          }
+        }, 60);
+      }
+    }
+  });
+})();
+
+// const time = 10000;
+// const step = 1;
+
+// function outNum(num, elem) {
+//   let e = document.querySelector(elem);
+//   let n = 0;
+//   let t = Math.round(time / (num / step));
+//   let interval = setInterval(() => {
+//     n = n + step;
+//     if (n == num) {
+//       clearInterval(interval);
+//     }
+//     e.innerHTML = n;
+//   }, t);
+// }
+
+// outNum(2005, '.number');
+// outNum(50, '.number1');
+// outNum(1500, '.number2');
+// outNum(100, '.number3');
 
 // var number = document.querySelector('.number'),
 //   numberTop = number.getBoundingClientRect().top,
